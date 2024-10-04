bf62d3d4b99fc963c8bcea4bd44e4c25445b4a02
MatildeFonsecaSimoes <shark05262@sharkcoders.pt>
2024-10-04 19:11:43 +0100

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,7 @@
+# Pergunta o nome do utilizador
+
+nome =input("Por favor, digite o seu nome:  ")
+
+print("ola,", nome )
+
+
---
+++ ex2.py
@@ -0,0 +1,11 @@
+# pergunta o nome do utilizador
+
+nome = input("por favor, digite o seu nome:  ")
+
+# pergunta o nome do curso
+
+curso = input("digite o nome do curso:  ")
+
+# Exibe a mensagem de ola com o nome e o curso
+print(f"ola, {nome}. És aluno do curso de {curso}")
+
